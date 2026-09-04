9465e86721dff471bee6dc2fea2b702a639c449f
Ihya' Ulumuddin <pebehakun45@gmail.com>
2026-09-04 21:05:07 +0700

    feat(ui): implement profile UI in MainActivity using Jetpack Compose

--- app/src/main/java/com/example/profilapp_245150201111024ihyaulumuddin/MainActivity.kt
+++ app/src/main/java/com/example/profilapp_245150201111024ihyaulumuddin/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.profilapp_245150201111024ihyaulumuddin.ui.theme.ProfilApp_245150201111024IhyaUlumuddinTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +32,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProfilApp_245150201111024IhyaUlumuddinTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.logo_universitas_brawijaya),
+                            contentDescription = "Foto Profil",
+                            modifier = Modifier.size(120.dp).clip(CircleShape)
+                        )
+                        Text("Nama: Andi", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                        Text("Mahasiswa Teknik Informatika")
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
                 }
             }
         }
